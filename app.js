fa328acd024b0524153fbd31c7d718fa8f1e4933
dknore <darcy.knore@gmail.com>
2017-11-04 17:08:50 -0700

    chart working

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 //Array to store images
 Image.allImages = [];
-// Image.container = document.getElementsByID('images');
 Image.justViewed = [];
+Image.container = document.getElementById('images');
 Image.results = document.getElementById('results');
-Image.TotalClicks = 0;
+Image.totalClicks = 0;
 
 //Object for images
 function Image(name, filepath) {
@@ -40,13 +40,10 @@ new Image('Wine Glass', 'images/wine-glass.jpg');
 
 //Add event listener when images is clicked AND
 var event1 = document.getElementById('photo1');
-event1.addEventListener('click', allPicks);
 event1.addEventListener('click', handleClick);
 var event2 = document.getElementById('photo2');
-event2.addEventListener('click', allPicks);
 event2.addEventListener('click', handleClick);
 var event3 = document.getElementById('photo3');
-event3.addEventListener('click', allPicks);
 event3.addEventListener('click',handleClick);
 
 //Random function
@@ -55,15 +52,14 @@ function randomImages() {
 }
 
 //call random function for images
-function allPicks() {
-  var img1Pick = randomImages();
-  var img2Pick = randomImages();
-  var img3Pick = randomImages();
+var img1Pick;
+var img2Pick;
+var img3Pick;
 
-  //Push into justViewed array
-  Image.justViewed.push(img1Pick);
-  Image.justViewed.push(img2Pick);
-  Image.justViewed.push(img3Pick);
+function allPicks() {
+  img1Pick = randomImages();
+  img2Pick = randomImages();
+  img3Pick = randomImages();
 
   //Make sure all images are truly random (different AND hasn't already been used
   while(img1Pick === img2Pick || img1Pick === img3Pick || Image.justViewed.includes(img1Pick)) {
@@ -76,33 +72,50 @@ function allPicks() {
     img3Pick = randomImages();
   }
 
-  //manipulate the DOM
+  //Push into justViewed array
+  Image.justViewed = [];
+  Image.justViewed.push(img1Pick);
+  Image.justViewed.push(img2Pick);
+  Image.justViewed.push(img3Pick);
+
+  //Take it to the DOM
   var imgEl1 = document.getElementById('photo1');
   imgEl1.src = Image.allImages[img1Pick].filepath;
+  Image.allImages[img1Pick].numTimesShown++;
 
   var imgEl2 = document.getElementById('photo2');
   imgEl2.src = Image.allImages[img2Pick].filepath;
+  Image.allImages[img2Pick].numTimesShown++;
 
   var imgEl3 = document.getElementById('photo3');
   imgEl3.src = Image.allImages[img3Pick].filepath;
+  Image.allImages[img3Pick].numTimesShown++;
 }
 allPicks();
 
-function handleClick() {
+function handleClick(event) {
   console.log('Clicks =', Image.totalClicks);
   if(Image.totalClicks > 24) {
     Image.container.removeEventListener('click', handleClick);
     createResults();
-  } else
-    Image.totalClicks++;
-
-  for(var i = 0; i < Image.name.length; i++) {
-    if(event.target.id === Image.allImages[i].name) {
-      Image.allImages[i].numTimesClicked++;
-      Image.allImages[i].numTimesShown++;
-      console.log(event.target.id + ' has ' + Image.allImages[i].numTimesClicked + ' votes in ' + Image.allImages[i].numTimesShown + ' views');
-    }
+    createChart();
   }
+  Image.totalClicks += 1;
+  console.log('EVENT', event);
+
+  if(event.target.id === 'photo1') {
+    Image.allImages[img1Pick].numTimesClicked += 1;
+    console.log(event.target.id + ' has ' + Image.allImages[img1Pick].numTimesClicked + ' votes in ' + Image.allImages[img1Pick].numTimesShown + ' views');
+  }
+  if(event.target.id === 'photo2') {
+    Image.allImages[img2Pick].numTimesClicked += 1;
+    console.log(event.target.id + ' has ' + Image.allImages[img2Pick].numTimesClicked + ' votes in ' + Image.allImages[img2Pick].numTimesShown + ' views');
+  }
+  if(event.target.id === 'photo3') {
+    Image.allImages[img3Pick].numTimesClicked += 1;
+    console.log(event.target.id + ' has ' + Image.allImages[img3Pick].numTimesClicked + ' votes in ' + Image.allImages[img3Pick].numTimesShown + ' views');
+  }
+  allPicks();
 }
 
 function createResults() {
@@ -112,4 +125,36 @@ function createResults() {
     Image.results.appendChild(liEl);
   }
 }
-createResults();
+
+//Making the chart
+function createChart() {
+  var data = [];
+  var names = [];
+  var ctx = document.getElementById('chart').getContext('2d');
+
+  for(var i = 0; i < Image.allImages.length; i++) {
+    data.push(Image.allImages[i].numTimesClicked);
+    names.push(Image.allImages[i].name);
+  }
+
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: names,
+      datasets: [{
+        label: '# of Votes',
+        data: data,
+        backgroundColor: '#e26441',
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
